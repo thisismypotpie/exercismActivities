912d123ee8b26931f34587a3159cbd764c27aece
Brandon Danielski <bld@pdx.edu>
2019-07-19 20:26:15 -0700

    Leap completed

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    // unimplemented!("true if {} is a leap year", year)
+    if year as i32 % 4 == 0 {
+        if year as i32 % 100 == 0 {
+            if year as i32 % 400 == 0 {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
