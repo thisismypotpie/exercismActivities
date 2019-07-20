5024341cedfe653ba4517e676843086ea5067316
Brandon Danielski <bld@pdx.edu>
2019-07-20 13:04:02 -0700

    collatz-conjecture finished

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,16 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    let mut steps: i32 = 0;
+    let mut sum: i32 = n as i32;
+    if n < 1 {
+        return None;
+    }
+    while sum != 1 {
+        if sum % 2 == 0 {
+            sum /= 2;
+        } else {
+            sum = (sum * 3) + 1;
+        }
+        steps += 1;;
+    }
+    Some(steps as u64)
 }
