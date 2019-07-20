7516e59eba3fe08b6c2cdb3910c081c1f1ec12fc
Brandon Danielski <bld@pdx.edu>
2019-07-20 08:51:11 -0700

    Proverb Completed

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,9 +1,16 @@
 pub fn build_proverb(_list: &[&str]) -> String {
     //unimplemented!("build a proverb from this list of items: {:?}", list)
-  let mut proverb = "";
+  let mut proverb = "".to_string();
   for i in 0.._list.len()
   {
-
+    if i == _list.len()-1
+    {
+      proverb+= &*format!("And all for the want of a {}.",_list[0]);
+    }
+    else
+    {
+      proverb+= &*format!("For want of a {} the {} was lost.\n",_list[i],_list[i+1]);
+    }
   }
 
   return proverb.to_string();
