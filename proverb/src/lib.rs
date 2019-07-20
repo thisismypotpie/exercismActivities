aef0e8d3045ace9404363949f53921516e042ed3
Brandon Danielski <bld@pdx.edu>
2019-07-19 22:24:30 -0700

    proverb update

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate rand;
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    //unimplemented!("build a proverb from this list of items: {:?}", list)
+  let mut index = rand::thread_rng();
+  return "boop"; 
 }
