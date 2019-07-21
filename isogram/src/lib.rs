e397aa89943a8c67c8a5854c8403918f9f9c515b
Brandon Danielski <bld@pdx.edu>
2019-07-20 17:24:44 -0700

    Isogram Complete

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn check(candidate: &str) -> bool {
-  for i in candidate.chars()
-  {
-    for j in candidate.chars()
-    {
-      if i == j && i !='-' && i != ' '
-      {
-	return false;
-      }
+    let mut letters = Vec::new();
+    for i in candidate.chars() {
+        // credit for the following one line found at: https://stackoverflow.com/questions/35432199/convert-a-char-to-upper-case
+        let test: Vec<_> = i.to_lowercase().collect();
+        if letters.contains(&test) && i != '-' && i != ' ' {
+            return false;
+        } else {
+            letters.push(test);
+        }
     }
-  }
-  true
+    true
 }
 //will always return false since each letter compares itself.
