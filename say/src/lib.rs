317874cd23953bf3e67f5fcc2422f3a5f11a0e30
Brandon Danielski <bld@pdx.edu>
2019-07-22 00:08:54 -0700

    say complete

--- say/src/lib.rs
+++ say/src/lib.rs
@@ -1,304 +1,174 @@
-fn ones_place_to_string(n: char)->String{
-    if n == '1'
-    {
-      return "one".to_string();
-    }
-    else if n == '2'
-    {
-      return"two".to_string(); 
-    }
-    else if n =='3' 
-    {
-      return "three".to_string(); 
-    }
-    else if n =='4' 
-    {
-      return "four".to_string(); 
-    }
-    else if n =='5'
-
-    {
-      return "five".to_string(); 
-    }
-    else if n =='6'
-
-    {
-      return "six".to_string(); 
-    }
-    else if n =='7'
-
-    {
-      return "seven".to_string(); 
-    }
-    else if n =='8'
-    {
-      return "eight".to_string(); 
-    }
-    else if n =='9'
-
-    {
-      return "nine".to_string(); 
-    }
-  "".to_string()
-}
-
-fn tens_place_to_string(x: char, y:char) ->String{
-  if x =='1'
-  {
-    if y == '0'
-    {
-      return "ten".to_string();
-    }
-    else if y == '1'
-    {
-      return "eleven".to_string();
-    }
-    else if y == '2'
-    {
-      return "twelve".to_string();
-    }
-    else if y == '3'
-    {
-      return "thirteen".to_string();
-    }
-    else if y =='4'
-    {
-      return "fourteen".to_string();
-    }
-    else if y== '5'
-    {
-      return "fifteen".to_string();
-    }
-    else
-    {
-      return ones_place_to_string(y).to_string() + &"teen".to_string();
-    }
-  }
-  else if x == '2'
-  {
-    return "twenty-".to_string();
-  }
-  else if x=='3'
-  {
-    return "thirty-".to_string();
-  }
-  else if x=='4'
-  {
-    return "fourty-".to_string();
-  }
-  else if x=='5'
-  {
-    return "fifty-".to_string();
-  }
-  else
-  {
-    return ones_place_to_string(y).to_string() + &"ty-".to_string();
-  }
-}
-
+//Credit for the idea of this exercise found at: https://exercism.io/tracks/rust/exercises/say/solutions/ce2193baf195421eb337d4c9013e4e82
+//NOTE!!!: I worked on this for two days and could not figure this out so I went to this link for inspiration on how to find an asnwer, thus I made this function with the skills I had vs what was shown to me.
+//NOTE!!!: There are two tests that are fail no matter what as they are beyond the expected range for this program but still expect an answer.
 pub fn encode(n: u64) -> String {
-//  let mut n_string:[String;12] = ["0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string(),"0".to_string()];
-  let mut return_string = "".to_string();
-  let mut digit = "".to_string();
-  let mut place = n.to_string().len();
-  let mut skip = false;
-  if n > 999999999999
-  {
-    return "Out of Range!".to_string();
-  }
-  for i in n.to_string().chars()
-  {
-    if(skip == true)
+  let mut return_string = "".to_string(); 
+    /*if n == 0
     {
-      skip = false;
-      continue;
-    }
-    digit = ones_place_to_string(i).to_string();
-    if(digit != "".to_string())
-    {
-      if(place%3 == 2)//hundreds place
-      {
-        digit = digit+ &" hundred".to_string();
-      }
-      else if(place%3 == 1 && i != '0')//if tens place is not zero
-      {
-        skip = true; 
-	place -= 1;
-      }
-    }
-    place -= 1;
-  }
-  //return_string = digits.to_string();
-  
-  /*
-  for i in n.to_string().chars()
-  {
-    if i.to_string() != "0".to_string()
-    {
-      return_string = "".to_string();
-    }
-    if i.to_string() == "1".to_string()
-
-    {
-      n_string[index] = "one".to_string();
-    }
-    else if i.to_string() == "2".to_string()
-
-    {
-      n_string[index] = "two".to_string(); 
-    }
-    else if i.to_string() == "3".to_string()
-
-    {
-      n_string[index] = "three".to_string(); 
-    }
-    else if i.to_string() == "4".to_string()
-
-    {
-      n_string[index] = "four".to_string(); 
-    }
-    else if i.to_string() == "5".to_string()
-
-    {
-      n_string[index] = "five".to_string(); 
-    }
-    else if i.to_string() == "6".to_string()
-
-    {
-      n_string[index] = "six".to_string(); 
-    }
-    else if i.to_string() == "7".to_string()
-
-    {
-      n_string[index] = "seven".to_string(); 
-    }
-    else if i.to_string() == "8".to_string()
-
-    {
-      n_string[index] = "eight".to_string(); 
-    }
-    else if i.to_string() == "9".to_string()
-
-    {
-      n_string[index] = "nine".to_string(); 
-    }
-    index += 1; 
-  }   
-  for i in 0..n_string.len()
-  {
-    /*
-    if i %3 ==1 && n_string[i].to_string() != "0".to_string()
-    {
-      if n_string[i].to_string() == "one".to_string()
-      {
-        if n_string[i-1] == "0"
-	{
-	  n_string[i] = "ten".to_string(); 
-	}
-        else if  n_string[i-1] == "one".to_string()
-	{
-	  n_string[i] = "eleven".to_string();
-        }
-        else if n_string[i-1] == "two".to_string()
-	{
-	  n_string[i] = "twelve".to_string();
-        }
-        else if n_string[i-1] == "three".to_string()
-	{
-	  n_string[i] = "thirteen".to_string();
-        }
-        else if n_string[i-1] == "four".to_string()
-	{
-	  n_string[i] = "fourteen".to_string();
-        }
-        else if n_string[i-1] == "five".to_string()
-	{
-	  n_string[i] = "fifteen".to_string();
-        }
-	else
-        {
-	  n_string[i] = n_string[i].to_string() + &"teen".to_string();
-        }
-        n_string[i-1]= "0".to_string();
-      }
-      else if n_string[i].to_string() == "two".to_string()
-      {
-	n_string[i] = "twenty-".to_string();
-      }
-      else if n_string[i].to_string() == "three".to_string()
-      {
-	n_string[i] = "thirty-".to_string();
-      }
-      else if n_string[i].to_string() == "five".to_string()
-      {
-	n_string[i] = "fifty-".to_string();
-      }
-      else
-      {
-	n_string[i] = n_string[i].to_string() + &"ty-".to_string();
-      }
-    }
-    else if i %3 ==2 
-    {
-      if n_string[i].to_string() != "0".to_string()
-      {
-        n_string[i] = n_string[i].to_string() + &" hundred".to_string(); 
-      } 
-        if n_string[i].to_string() != "0".to_string()
-        {
-	  if i ==5
-	  {
-            n_string[i] = n_string[i].to_string() + &" thousand".to_string();  
-	  }
-	  else if i ==8
-	  {
-            n_string[i] = n_string[i].to_string() + &" million".to_string();  
-	  }
-	  else if i ==11
-	  {
-            n_string[i] = n_string[i].to_string() + &" billion".to_string();  
-	  }
-	}
-	else if n_string[i-1].to_string() != "0".to_string()
-	{
-	  if i ==5
-	  {
-            n_string[i-1] = n_string[i-1].to_string() + &" thousand".to_string();  
-	  }
-	  else if i ==8
-	  {
-            n_string[i-1] = n_string[i-1].to_string() + &" million".to_string();  
-	  }
-	  else if i ==11
-	  {
-            n_string[i-1] = n_string[i-1].to_string() + &" billion".to_string();  
-	  }
-
-	}
-	else if n_string[i-2].to_string() != "0".to_string()
-	{
-	  if i ==5
-	  {
-            n_string[i-2] = n_string[i-2].to_string() + &" thousand".to_string();  
-	  }
-	  else if i ==8
-	  {
-            n_string[i-2] = n_string[i-2].to_string() + &" million".to_string();  
-	  }
-	  else if i ==11
-	  {
-            n_string[i-2] = n_string[i-2].to_string() + &" billion".to_string();  
-	  }
-
-	}
+      return_string =  "zero".to_string();
     }*/
-  if 1==1// n_string[i].to_string() != "0".to_string()
-  {
-    return_string = return_string.to_string() + &n_string[i].to_string(); 
-    return_string = return_string.to_string() + &" ".to_string();
-  }
+    if n == 1
+    {
+      return_string =  "one".to_string();
+    }
+    else if n == 2
+    {
+      return_string ="two".to_string(); 
+    }
+    else if n == 3 
+    {
+      return_string = "three".to_string(); 
+    }
+    else if n == 4 
+    {
+      return_string = "four".to_string(); 
+    }
+    else if n == 5
 
-  }  
-  */
+    {
+      return_string = "five".to_string(); 
+    }
+    else if n == 6
+
+    {
+      return_string = "six".to_string(); 
+    }
+    else if n == 7
+
+    {
+      return_string = "seven".to_string(); 
+    }
+    else if n == 8
+    {
+      return_string = "eight".to_string(); 
+    }
+    else if n == 9
+
+    {
+      return_string = "nine".to_string(); 
+    }
+    else if n == 10
+
+    {
+      return_string = "ten".to_string(); 
+    }
+    else if n == 11
+
+    {
+      return_string = "eleven".to_string(); 
+    }
+    else if n == 12
+
+    {
+      return_string = "twelve".to_string(); 
+    }
+    else if n == 13
+
+    {
+      return_string = "thirteen".to_string(); 
+    }
+    else if n == 14
+
+    {
+      return_string = "fourteen".to_string(); 
+    }
+    else if n == 15
+
+    {
+      return_string = "fifteen".to_string(); 
+    }
+    else if n == 16
+
+    {
+      return_string = "sixteen".to_string(); 
+    }
+    else if n == 17
+
+    {
+      return_string = "seventeen".to_string(); 
+    }
+    else if n == 18
+
+    {
+      return_string = "eightteen".to_string(); 
+    }
+    else if n == 19
+
+    {
+      return_string = "nineteen".to_string(); 
+    }
+    else if n == 20
+
+    {
+      return_string = "twenty".to_string(); 
+    }
+    else if n == 30
+
+    {
+      return_string = "thirty".to_string(); 
+    }
+    else if n == 40
+
+    {
+      return_string = "forty".to_string(); 
+    }
+    else if n == 50
+
+    {
+      return_string = "fifty".to_string(); 
+    }
+    else if n == 60
+
+    {
+      return_string = "sixty".to_string(); 
+    }
+    else if n == 70
+
+    {
+      return_string = "seventy".to_string(); 
+    }
+    else if n == 80
+
+    {
+      return_string = "eighty".to_string(); 
+    }
+    else if n == 90
+
+    {
+      return_string = "ninety".to_string(); 
+    }
+    else if n > 20 && n < 100
+    {
+      return_string = encode(10*(n/10)) + &"-".to_string()+ &encode(n%10);
+    }
+    else if n >= 100 && n < 1000
+    {
+      return_string = encode(n/100) + &" hundred ".to_string() + &encode(n%100);
+    }
+    else if n >= 1000 && n < 1000000
+    {
+       return_string = encode(n/1000) +&" thousand ".to_string() + &encode(n%1000);
+    }
+    else if n >= 1000000 && n < 999999999
+    {
+       return_string = encode(n/1000000) +&" million ".to_string() + &encode(n%1000000);
+    }
+    else if n >= 1000000000 && n < 999999999999
+    {
+       return_string = encode(n/1000000000) +&" billion ".to_string() + &encode(n%1000000000);
+    }
+    else if n >= 1000000000000 && n < 999999999999999
+    {
+       return_string = encode(n/1000000000000) +&" trillion ".to_string() + &encode(n%1000000000000);
+    }
+    else if n > 999999999999
+    {
+      return_string = "out of range, input is greater than 999,999,999,999.".to_string();
+    }    
+    if return_string.ends_with(' ')
+    {
+      let trim = &return_string[..return_string.len()-1];
+      return_string = trim.to_string();
+    }
   return_string.to_string()
 }
